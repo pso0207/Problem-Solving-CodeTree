beb697589af44d6f96a66279674788a5218be00f
ParkSangwon <129036559+pso0207@users.noreply.github.com>
2026-04-27 21:30:55 +0900

    Title: 이상한 진수 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260427/이상한 진수 2/awkward-digits-2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+bool is_odd(int a)
+{
+    if(a % 2 == 0) return false;
+    else return true;
+}
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin >> a;
+    int result = 0;
+    int mul = 1;
+    vector<int> temp;
+    while(a != 0)
+    {
+        if(!is_odd(a)) temp.push_back(0);
+        else temp.push_back(1);
+        a /= 10;
+    }
+    for(int i = temp.size()-1; i > 0; i--)
+    {
+        if(temp[i-1] == 0 ) temp[i-1] = 1;
+        break;
+    }
+    for(int& i : temp)
+    {
+        result += i * mul;
+        mul *=2;
+    }
+    cout << result;
+}
