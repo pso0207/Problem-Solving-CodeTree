fa9a0df6f8c96b30f522b67785e222931685923b
ParkSangwon <129036559+pso0207@users.noreply.github.com>
2026-07-15 14:11:16 +0900

    [Passed] 훌륭한 점프 (2ms, 0MB)

--- trail2/훌륭한 점프/great-jump.cpp
+++ trail2/훌륭한 점프/great-jump.cpp
@@ -1,42 +1,38 @@
 #include <iostream>
-#include <vector>
 #include <climits>
 using namespace std;
-int K, N;
-vector<int> num;
 
-bool is_possible(int max_num)
-{
+int K;
+int N;
+int num[100];
+
+bool is_possible(int max_num){
     int last_idx = 0;
+
     for(int i = 0; i < N; i++)
     {
-        if(num[i] <= max_num)
-        {
-            if(i - last_idx > K)
-                return false;
-            last_idx = i;
+        if(num[i] > max_num) continue;
+        else{
+            if(i - last_idx > K) return false;
+            else last_idx = i;
         }
     }
     return true;
 }
 
 int main() {
-    // Please write your code here
+    // Please write your code here.
     cin >> N >> K;
-    int min_num = INT_MAX;
+    int result = INT_MAX;
     for(int i = 0; i < N; i++)
     {
-        int temp;
-        cin >> temp;
-        num.push_back(temp);
+        cin >> num[i];
     }
-    // 처음과 끝은 무조건 지나는 지점이다
-    // 최댓값의 최소이기 때문에 이 지점보다 작을 순 없다.
-    for(int i = max(num[0], num[N-1]); i <= 100 ; i++){
-        if(is_possible(i)) min_num = min(i,min_num);
+    for(int i = max(num[0], num[N-1]); i <= 100; i++){
+        if(is_possible(i)){
+            result = min(result, i);
+        }
     }
-    cout << min_num;
-
-
+    cout << result;
     return 0;
 }
