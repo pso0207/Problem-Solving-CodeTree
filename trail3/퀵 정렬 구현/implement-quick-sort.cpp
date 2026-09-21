82ffebd4aa2b2b433707025a8087603be580bcf0
ParkSangwon <129036559+pso0207@users.noreply.github.com>
2026-09-21 22:05:19 +0900

    [Passed] 퀵 정렬 구현 (32ms, 1MB)

---
+++ trail3/퀵 정렬 구현/implement-quick-sort.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> num(n);
+    for(int i =0 ; i < n ;i++){
+        cin >> num[i];
+    }
+    sort(num.begin(),num.end());
+    for(auto n : num){
+        cout << n << " ";
+    }
+    return 0;
+}
